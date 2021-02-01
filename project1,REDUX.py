4e9b418b8915c6ec7a2970d45907ce31be0c44e1
mrdc1790 <padam257@gmail.com>
2021-02-01 15:48:24 -0500

    final

--- project1,REDUX.py
+++ project1,REDUX.py
@@ -1,17 +1,24 @@
+#   By: Adam Pollack
+#	E02063895
+#   Created: 1/27/2021
+#   For: COSC 314 - Discrete Mathematics
+#	Title: Project 1: Logical Expressions Evaluator
+#   Function: Takes a logical expression and evaluates the truth value - True or False
+
 print("Logical Expressions Evaluator")
 print("-----------------------------")
 numVar = int(input("Enter the number of variables in the expression: "))
 
-def trueOrFalse(x):
+def trueOrFalse(x): #used to return a boolean value when given a string
 	if x.startswith('f' or 'F'):
 		return False
 	if x.startswith('t' or 'T'):
 		return True
-	if not x.startswith('t' or 'T' or 'F' or 'f'):
+	if not x.startswith('t' or 'T' or 'F' or 'f'): #used to replicate if given wrong input
 		print("Please enter a valid value")
 		findNumVar(numVar)
 
-def findNumVar(numVar):
+def findNumVar(numVar): #used to return a boolean value to the respective variable when given a certain number of inputs
 	print("Enter the values of variables - true or false: ")
 	global numA, numB, numC, numD, numE
 	global a, b, c, d, e
@@ -58,10 +65,11 @@ def findNumVar(numVar):
 
 findNumVar(numVar)
 exp = input("Enter a logical expression: ")
-boolArray = exp.split(" ")
-hi = ""
+boolArray = exp.split(" ")  #take the input (string) and converts it to an array
+hi = ""   #initalize a string to pass booleans through
 
-for i in range(len(boolArray)):
+for i in range(len(boolArray)): #the bulk of the programs work is done here; evaluates which variables are used in the expression
+								# and stores the string version of the boolean within the preexisting spot in the array
 	if ("and" != boolArray[i]):
 		boolArray[i] = boolArray[i].replace('n', "not ")
 	if ('a' == boolArray[i]):
@@ -74,16 +82,15 @@ for i in range(len(boolArray)):
 		boolArray[i] = str(numD)
 	elif ('e' == boolArray[i]):
 		boolArray[i] = str(numE)
-	elif ("n" == boolArray[i]):
-		boolArray[i] = boolArray[i].replace("n", "not ")
-	if ("and" != boolArray[i] and "or" != boolArray[i] and 'n' in boolArray[i]):
+	if ("and" != boolArray[i] and "or" != boolArray[i] and 'n' in boolArray[i]): #condition to deal with operators and not screw up "and" cuz it has an 'a' in it
 		hi = str(eval(boolArray[i])) + " "
-		hi = not eval (hi)
+		hi = not eval(hi)
 	if ("not" in boolArray[i]):
 		boolArray[i] = str(hi)
 
 sentence = " ".join(boolArray)
 print(sentence)
+print(boolArray)
 answer = eval(sentence)
 
 if (answer == True):
